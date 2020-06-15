416993e14da0de7e5426139f0f0113ee712ece98
Martin <mocchi@certisur.com>
2020-06-14 22:31:09 -0300

    prueba de llamado a python

--- src/main/java/com/proyecto/skinnerServer/rest/RestData.java
+++ src/main/java/com/proyecto/skinnerServer/rest/RestData.java
@@ -1,5 +1,8 @@
 package com.proyecto.skinnerServer.rest;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,7 +22,31 @@ public class RestData {
 	public Map<String, Object> greeting() {
 		Map<String, Object> map = new HashMap<String, Object>();
         map.put("status", 200);
-        map.put("message", "Respuesta de SpringBoot");
+        //map.put("message", "Respuesta de SpringBoot");
+        try {
+            String s = "ANÁLISIS DE IMAGEN - RESULTADO: ";
+            System.out.println(System.getProperty("user.dir"));
+            //Process p = Runtime.getRuntime().exec("python3 " + System.getProperty("user.dir") + "/ProyectoSkinner/RedCNN/Red2/CNN.py");
+            Process p = Runtime.getRuntime().exec("python3 " + System.getProperty("user.dir") + "/src/main/resources/hello.py")	;
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            System.out.println("Waiting for batch file ...");
+            try {
+				p.waitFor();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("TODO MAL ESPERANDO");
+			}
+            System.out.println("Batch file done.");
+            
+            while((s = in.readLine()) !=null){
+            	map.put("message", s);
+            	s = in.readLine();
+        	}
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
         return map;
 //		return "Son 5, chicos... sta bien? y los MP? ehh? sta bien? y si yo me saco la foto adentro del ba�o? sin luz? sta bien? anda?";
 	}
