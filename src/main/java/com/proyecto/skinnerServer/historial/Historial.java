ccc2b421e7f373f889704492acd946f79a961d85
Facundo Petrolo <petrolo.f@gmail.com>
2020-08-10 21:15:40 -0300

    corregida importacion

--- src/main/java/com/proyecto/skinnerServer/historial/Historial.java
+++ src/main/java/com/proyecto/skinnerServer/historial/Historial.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import helper.Helper;
 
