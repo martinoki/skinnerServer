ae488538c1f3d5a21efdee3f1f69c15bf6bc84f7
Martin <mocchi@certisur.com>
2020-09-14 18:00:21 -0300

    merge

---
+++ src/main/java/com/proyecto/skinnerServer/users/Post.java
@@ -0,0 +1,13 @@
+package com.proyecto.skinnerServer.users;
+
+import java.util.Map;
+
+public class Post {
+	 private long multicast_id;
+	    private int success;
+	    private int failure;
+	    private int canonical_ids;
+	    private Map<String, Object> results;
+
+	    // getters and setters
+}
