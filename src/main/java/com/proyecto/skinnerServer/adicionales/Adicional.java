75323653c35aad7c879ef822a36fe835b39c1fc1
Facundo Petrolo <petrolo.f@gmail.com>
2020-09-24 00:04:10 -0300

    Agregadas llamadas a adicionales

---
+++ src/main/java/com/proyecto/skinnerServer/adicionales/Adicional.java
@@ -0,0 +1,90 @@
+package com.proyecto.skinnerServer.adicionales;
+
+import java.util.List;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import helper.Helper;
+
+@RestController
+//@RequestMapping(path="/")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
+
+public class Adicional {
+	
+	 @Autowired
+	 JdbcTemplate jdbcTemplate;
+
+	@GetMapping("/adicionales/{id}")
+	public List<Map<String,Object>> getAsignacionesPorId(@PathVariable("id") long id){
+		String sql = "SELECT * FROM adicionales WHERE id_historial= %d order by id_tipo asc";
+		sql = String.format(sql, id);
+		List<Map<String,Object>>listaResultado= jdbcTemplate.queryForList(sql);
+		List<Map<String,Object>>listaResultadoAgrupado = new ArrayList<Map<String,Object>>();
+       	Map<String,Object> grupo=new HashMap<String, Object>();
+		int idx = 1;
+		for (Map<String,Object> item : listaResultado) {
+				if(idx==(int)item.get("id_tipo")) {
+            	grupo.put("id_tipo", (int)item.get("id_tipo"));
+            	if(item.get("tipo").toString().equals("Borde")) {
+                	grupo.put("borde",item.get("imagen"));
+            	}
+            	if(item.get("tipo").toString().equals("Color")) {
+                	grupo.put("color",item.get("imagen"));
+            	}
+            	if(item.get("tipo").toString().equals("Recortada")) {
+                	grupo.put("recortada",item.get("imagen"));
+            	}
+            }
+            else {
+            	idx++;
+            	listaResultadoAgrupado.add(grupo);
+                    grupo=new HashMap<String, Object>();
+
+	            	grupo.put("id_tipo", (int)item.get("id_tipo"));
+	            	if(item.get("tipo").toString().equals("Borde")) {
+	                	grupo.put("borde",item.get("imagen"));
+	            	}
+	            	if(item.get("tipo").toString().equals("Color")) {
+	                	grupo.put("color",item.get("imagen"));
+	            	}
+	            	if(item.get("tipo").toString().equals("Recortada")) {
+	                	grupo.put("recortada",item.get("imagen"));
+	            	}
+	            
+            }
+
+		}
+    	listaResultadoAgrupado.add(grupo);
+return listaResultadoAgrupado;
+//TODO Hacer un mejor codigo By:luqui
+//BLAME LUQUI
+	}
+
+	@DeleteMapping("/adicionales/{id}")
+	public Map<String,Object> deleteAdicional(@PathVariable("id") long id){
+		String sql = "delete from adicionales WHERE id = %d;";
+		sql = String.format(sql, id);
+		Map<String, Object> map = new HashMap<String, Object>();
+        map.put("status", 200);
+        return map;
+	}
+}
