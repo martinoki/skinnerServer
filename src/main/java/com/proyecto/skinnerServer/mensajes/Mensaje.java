7baf68b8cd377c833b90bb451852b39d6ab284a7
Facundo Petrolo <petrolo.f@gmail.com>
2020-10-20 20:16:43 -0300

    Modificada query de mensajes

--- src/main/java/com/proyecto/skinnerServer/mensajes/Mensaje.java
+++ src/main/java/com/proyecto/skinnerServer/mensajes/Mensaje.java
@@ -53,7 +53,7 @@ public class Mensaje {
 		
 		@GetMapping("/mensajes/paciente/{idPaciente}")
 		public List<Map<String,Object>> getMensajesByIdPaciente(@PathVariable("idPaciente") long id){
-			String sql = "SELECT a.id_origen_usuario, a.id_lesion, MAX(a.fecha) as fecha,MAX(c.nombre) as nombre_destino,MAX(c.apellido) as apellido_destino,MAX(a.mensaje) as mensaje,MAX(a.id_destino_usuario) as id_destino_usuario FROM mensajes a join usuarios c on a.id_destino_usuario=c.id WHERE a.id_origen_usuario= %d GROUP BY a.id_origen_usuario, a.id_lesion ORDER BY MAX(a.fecha) DESC";
+			String sql = "Select id_origen_usuario, id_lesion, fecha, nombre_destino,apellido_destino,mensaje, id_destino_usuario, l.descripcion as descripcion,l.imagen as imagen from (SELECT a.id_origen_usuario as id_origen_usuario, a.id_lesion as id_lesion, MAX(a.fecha) as fecha,MAX(c.nombre)as nombre_destino,MAX(c.apellido) as apellido_destino,MAX(a.mensaje) as mensaje,MAX(a.id_destino_usuario) as id_destino_usuario FROM mensajes a join usuarios c on a.id_destino_usuario=c.id WHERE a.id_origen_usuario= %d GROUP BY a.id_origen_usuario, a.id_lesion ORDER BY MAX(a.fecha) DESC) as m join lesiones l on m.id_lesion=l.id order by fecha desc";
 			sql = String.format(sql, id);
 			return jdbcTemplate.queryForList(sql);
 		}
